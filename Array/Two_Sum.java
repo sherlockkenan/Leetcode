03963ddf7829eaff0b6142695413b3eb00248ff0
KeXH <sherlockkenan@outlook.com>
2018-10-15 16:40:28 +0800

    Update and rename Two_Sum.cpp to Two_Sum.java

---
+++ Array/Two_Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hashmap = new HashMap();
+        for(int i=0; i< nums.length; i++) {
+            hashmap.put(nums[i], i);
+        }
+        for(int i=0; i <nums.length; i++){
+            int complement = target - nums[i];
+            if(hashmap.containsKey(complement)&& hashmap.get(complement)!=i){
+                return new int[]{i,hashmap.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
